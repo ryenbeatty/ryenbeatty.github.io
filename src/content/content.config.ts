55089a9ea619fa1860831a1c483c4292596263ad
ryenbeatty <mail@ryenbeatty.info>
2024-02-16 15:45:32 +0000

    feat: init

---
+++ src/content/content.config.ts
@@ -0,0 +1,21 @@
+import { defineCollection, z } from 'astro:content'
+
+const blogCollection = defineCollection({ 
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+  }),
+ });
+
+ const projectCollection = defineCollection({
+  type: 'content',
+  schema: z.object({})
+ })
+
+export const collections = {
+  'blog': blogCollection,
+  'project': projectCollection
+  // 'newsletter': blogCollection,
+}
