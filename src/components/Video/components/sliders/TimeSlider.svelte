30e4c36094052d4c0db0f8be63545614276058db
ryenbeatty <mail@ryenbeatty.info>
2024-02-25 15:38:40 +0000

    feat: page structure and basic styling

---
+++ src/components/Video/components/sliders/TimeSlider.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import SliderChapters from './SliderChapters.svelte'
+</script>
+
+<media-time-slider class="media-slider">
+  <SliderChapters />
+</media-time-slider>
+
+<style>
+  .media-slider {
+    display: inline-flex;
+    align-items: center;
+    width: 100%;
+    height: 40px;
+    position: relative;
+    contain: layout style;
+    outline: none;
+    pointer-events: auto;
+    cursor: pointer;
+    user-select: none;
+    touch-action: none;
+    /** Prevent thumb flowing out of slider (15px = thumb width). */
+    margin: 0 calc(15px / 2);
+    -webkit-user-select: none;
+    -webkit-tap-highlight-color: transparent;
+  }
+</style>
--- src/content/project/test-study.md
+++
@@ -1,7 +0,0 @@
----
-
-title: Test study
-
----
-
-The study
